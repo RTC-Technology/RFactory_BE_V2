37b26e7f4331d1f0c82420eb0b3a6245a1c25f82
LAP177 <phungchien30031993@gmail.com>
2026-08-12 17:44:59 +0700

    -  Thêm API get ds warehouse, location

--- RFactory.Application/Modules/GoodsReceipt/Services/IGoodsReceiptServices.cs
+++ RFactory.Application/Modules/GoodsReceipt/Services/IGoodsReceiptServices.cs
@@ -20,10 +20,12 @@ namespace RFactory.Application.Modules.GoodsReceipt.Services
 
     public interface IGoodsReceiptDetailServices
     {
-        Task<List<GoodsReceiptDetailDto>> GetAllAsync(CancellationToken ct = default);
+        Task<List<GoodsReceiptDetailDto>> GetAllAsync(long? receiptId, CancellationToken ct = default);
         Task<GoodsReceiptDetailDto?> GetByIdAsync(ulong id, CancellationToken ct = default);
         Task<Result<GoodsReceiptDetailDto>> CreateAsync(CreateGoodsReceiptDetailRequest request, CancellationToken ct = default);
+        Task<Result<List<GoodsReceiptDetailDto>>> CreateRangeAsync(List<CreateGoodsReceiptDetailRequest> requests, CancellationToken ct = default);
         Task<Result<GoodsReceiptDetailDto>> UpdateAsync(ulong id, UpdatesGoodsReceiptDetailRequest request, CancellationToken ct = default);
+        Task<Result<List<GoodsReceiptDetailDto>>> UpdateRangeAsync(List<UpdatesGoodsReceiptDetailRequest> requests, CancellationToken ct = default);
         Task<Result> DeleteAsync(ulong id, CancellationToken ct = default);
     }
 }
